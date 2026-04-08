a22202e575383a64b18d39c36649048789683ce1
Hans S. Manguilimotan <130669271+hansmanguilimotan-devworks@users.noreply.github.com>
2026-04-08 20:07:53 +0800

    Add files via upload

--- website_b_elt/app.py
+++ website_b_elt/app.py
@@ -304,4 +304,5 @@ def sync():
 init_db()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5002)))
+    port = int(os.environ.get("PORT", 5002))
+    app.run(host="0.0.0.0", port=port)
